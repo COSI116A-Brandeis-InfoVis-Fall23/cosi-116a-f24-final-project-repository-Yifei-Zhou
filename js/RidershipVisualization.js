a4e28496b5d47f866d58ce3fa401f9a631248097
Randylight <randylight111@gmail.com>
2024-12-02 22:57:42 -0500

    Add scales and axes to the line chart

--- js/RidershipVisualization.js
+++ js/RidershipVisualization.js
@@ -11,3 +11,18 @@ const svg = d3.select("body").append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
+
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y);
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
