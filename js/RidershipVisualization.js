85d9d057ada5b6b6544b4c3d240c8a345dd72be4
Yifei-Zhou <yfzhou23@gmail.com>
2024-12-16 20:32:14 -0500

    Try to fix Qifeng codes, failed

--- js/RidershipVisualization.js
+++ js/RidershipVisualization.js
@@ -1,127 +1,125 @@
-d3.csv("/mnt/data/MBTA_Commuter_Rail_Ridership_by_Trip%2C_Season%2C_Route_Line%2C_and_Stop.csv").then(function(data) {
-    console.log("Data loaded:", data);
-});
+function ridershipVisualizationChart(config) {
+    const { width, height, container } = config;
+    const margin = { top: 50, right: 150, bottom: 70, left: 70 };
 
-const margin = {top: 20, right: 20, bottom: 30, left: 50};
-const width = 960 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
+    const svgWidth = width + margin.left + margin.right;
+    const svgHeight = height + margin.top + margin.bottom;
 
-const svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-const x = d3.scaleTime().range([0, width]);
-const y = d3.scaleLinear().range([height, 0]);
-
-const xAxis = d3.axisBottom(x);
-const yAxis = d3.axisLeft(y);
-
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-
-
-
-data.forEach(function(d) {
-    d.date = new Date(d.date);
-    d.ridership = +d.ridership;
-});
-
-x.domain(d3.extent(data, function(d) { return d.date; }));
-y.domain([0, d3.max(data, function(d) { return d.ridership; })]);
-
-
-
-const line = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.ridership); });
-
-svg.append("path")
-    .data([data])
-    .attr("class", "line")
-    .attr("d", line);
-
-    const brush = d3.brushX()
-    .extent([[0, 0], [width, height]])
-    .on("end", brushEnded);
-
-svg.append("g")
-    .attr("class", "brush")
-    .call(brush);
-
-function brushEnded(event) {
-    const selection = event.selection;
-    if (selection) {
-        const [start, end] = selection.map(x.invert);
-        const filteredData = data.filter(d => d.date >= start && d.date <= end);
-        console.log("Brushed data:", filteredData);
+    const containerElement = d3.select(container);
+    if (containerElement.empty()) {
+        console.error("Container element not found: ", container);
+        return;
     }
-}
 
+    containerElement.selectAll("svg").remove(); // Clear previous SVG if it exists
 
+    const svg = containerElement.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-const zoom = d3.zoom()
-    .scaleExtent([1, 10])
-    .translateExtent([[0, 0], [width, height]])
-    .on("zoom", zoomed);
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
 
-svg.call(zoom);
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
 
-function zoomed(event) {
-    const transform = event.transform;
-    const newX = transform.rescaleX(x);
-    svg.select(".x.axis").call(xAxis.scale(newX));
-    svg.select(".line").attr("d", line.x(d => newX(d.date)));
-}
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
-svg.selectAll("circle")
-    .data(data)
-    .enter().append("circle")
-    .attr("cx", d => x(d.date))
-    .attr("cy", d => y(d.ridership))
-    .attr("r", 3)
-    .on("mouseover", function(event, d) {
-        d3.select(this).attr("r", 6).style("fill", "red");
-    })
-    .on("mouseout", function() {
-        d3.select(this).attr("r", 3).style("fill", "black");
+    d3.csv("../data/MBTA_Commuter_Rail_Ridership_by_Trip_Season_Route_Line_and_Stop.csv").then(function(data) {
+        console.log("Data loaded:", data);
+
+        // Now process data
+        data.forEach(function(d) {
+            d.date = new Date(d.date);
+            d.ridership = +d.ridership;
+        });
+
+        x.domain(d3.extent(data, d => d.date));
+        y.domain([0, d3.max(data, d => d.ridership)]);
+
+        const line = d3.line()
+            .x(d => x(d.date))
+            .y(d => y(d.ridership));
+
+        svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .attr("d", line);
+
+        // Add circles
+        svg.selectAll("circle")
+            .data(data)
+            .enter().append("circle")
+            .attr("cx", d => x(d.date))
+            .attr("cy", d => y(d.ridership))
+            .attr("r", 3)
+            .on("mouseover", function(event, d) {
+                d3.select(this).attr("r", 6).style("fill", "red");
+            })
+            .on("mouseout", function() {
+                d3.select(this).attr("r", 3).style("fill", "black");
+            });
+
+        // Add season filter dropdown
+        d3.select("body").append("select")
+            .attr("id", "seasonFilter")
+            .selectAll("option")
+            .data([...new Set(data.map(d => d.season))])
+            .enter().append("option")
+            .text(d => d)
+            .on("change", function() {
+                const selectedSeason = d3.select("#seasonFilter").property("value");
+                const filteredData = data.filter(d => d.season === selectedSeason);
+                updateChart(filteredData);
+            });
+
+        function updateChart(filteredData) {
+            x.domain(d3.extent(filteredData, d => d.date));
+            y.domain([0, d3.max(filteredData, d => d.ridership)]);
+
+            svg.select(".x.axis").call(xAxis);
+            svg.select(".y.axis").call(yAxis);
+
+            svg.select(".line").data([filteredData]).attr("d", line);
+        }
+
+        function brushEnded(event) {
+            const selection = event.selection;
+            if (selection) {
+                const [start, end] = selection.map(x.invert);
+                const filteredData = data.filter(d => d.date >= start && d.date <= end);
+                console.log("Brushed data:", filteredData);
+            }
+        }
+    
+        const zoom = d3.zoom()
+            .scaleExtent([1, 10])
+            .translateExtent([[0, 0], [width, height]])
+            .on("zoom", zoomed);
+    
+        svg.call(zoom);
+    
+        function zoomed(event) {
+            const transform = event.transform;
+            const newX = transform.rescaleX(x);
+            svg.select(".x.axis").call(xAxis.scale(newX));
+            svg.select(".line").attr("d", line.x(d => newX(d.date)));
+        }
+
+        svg.on("click", function() {
+            // Clicking on blank areas will clear highlights
+            svg.selectAll("circle").attr("r", 3).style("fill", "black");
+        });
+
+        console.log("Visualization complete");
     });
-
-
-    d3.select("body").append("select")
-    .attr("id", "seasonFilter")
-    .selectAll("option")
-    .data([...new Set(data.map(d => d.season))])
-    .enter().append("option")
-    .text(d => d)
-    .on("change", function() {
-        const selectedSeason = d3.select("#seasonFilter").property("value");
-        const filteredData = data.filter(d => d.season === selectedSeason);
-        updateChart(filteredData);
-    });
-
-function updateChart(filteredData) {
-    x.domain(d3.extent(filteredData, d => d.date));
-    y.domain([0, d3.max(filteredData, d => d.ridership)]);
-
-    svg.select(".x.axis").call(xAxis);
-    svg.select(".y.axis").call(yAxis);
-
-    svg.select(".line").data([filteredData]).attr("d", line);
 }
-
-
-svg.on("click", function() {
-    // Clicking on blank areas will clear highlights
-    svg.selectAll("circle").attr("r", 3).style("fill", "black");
-});
-
-console.log("Visualization complete");
--- js/visualization.js
+++ js/visualization.js
@@ -17,6 +17,12 @@ const ridershipHeatmapConfig = {
   container: ".ridership-heatmap"
 };
 
+const ridershipvisualConfig = {
+  width: 1440,
+  height: 480,
+  container: ".ridershipvisual-chart"
+};
+
 // Function to render all visualizations
 function renderVisualizations() {
   // Select the container element for the visualizations
@@ -26,6 +32,7 @@ function renderVisualizations() {
   visHolder.append("div").attr("class", "railRidershipByTimePeriod-chart");
   visHolder.append("div").attr("class", "monthlyRidershipByMode-chart");
   visHolder.append("div").attr("class", "ridership-heatmap");
+  visHolder.append("div").attr("class", "ridershipvisual-chart");
 
   // Render each visualization
   d3.csv("./data/Fall_2023_MBTA_Rail_Ridership_Data_by_SDP_Time,_Period_Route_Line,_and_Stop.csv").then(data => {
@@ -54,6 +61,7 @@ function renderVisualizations() {
     });
   });
   monthlyRidershipByModeChart(monthlyRidershipByModeChartConfig);
+  ridershipVisualizationChart(ridershipvisualConfig);
 }
 
 // Execute the rendering function when the document is ready
